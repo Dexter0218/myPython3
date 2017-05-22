a106fe96bfc8a78f15a517e3058f1ad9e0b0479d
dexter0218 <wupeng218@126.com>
2017-05-22 18:58:53 +0800

    增加原始图片下载

--- getLifeOfPix.py
+++ getLifeOfPix.py
@@ -7,10 +7,11 @@ urls="http://www.lifeofpix.com/"
 
 desPath = r'F:\pythonWork'
 
+
 def gotoDir(dir):
 
     if os.path.exists(dir):#Checks if the dir exists
-        print("The directory exists")
+        print("The directory: %s exists"%dir)
     else:
         print("No directory found for "+dir) #Output if no directory
         print
@@ -34,11 +35,11 @@ def getTotalPage(html):
     #pattern = re.compile('<img src=".*?"')
     result = re.findall(pattern, html)
     num = re.sub(r'\D', "", result[0])
-    print(num)
+    print("一共需要下载 %s 页的图片"%num)
     return int(num)
 
 
-def getPic(html):
+def getPreviewPic(html):
     pattern = re.compile('<img src="[a-zA-z]+://[^\s]*.jpg"',re.S)
     result = re.findall(pattern, html)
     # print(result)
@@ -50,23 +51,68 @@ def getPic(html):
         # print(image)
         path = image[0].replace("src=\"","")
         # print(path)
-
         p,filename=os.path.split(path) 
         # print(filename)
         if not os.path.exists(filename):
-            print("dir is:" + path)  
-            print("file is:" + filename) 
+            
             if not (filename.endswith("150x150.jpg")):
-                print("下载中，耐心等待") 
+                print("下载链接:" + path)  
+                print("文件名:" + filename) 
+                print("下载小图中，请耐心等待...") 
                 urllib.request.urlretrieve(path, filename)
         else:
             print("已经存在"+filename)
 
+def getDownloadPic(html):
+    pattern = re.compile('<a download="[a-zA-z]+://[^\s]*.jpg"',re.S)
+    result = re.findall(pattern, html)
+    print(result)
+
+    for item in result:
+        imagepattern = re.compile('download="[^\s]*.jpg',re.S)
+        image = re.findall(imagepattern, item)
+        # print(image)
+        path = image[0].replace("download=\"","")
+        # print(path)
+
+        p,filename=os.path.split(path) 
+        # print(filename)
+        if not os.path.exists(filename):
+            if not (filename.endswith("150x150.jpg")):
+                print("下载链接:" + path)  
+                print("文件名:" + filename) 
+                print("下载大图中，请耐心等待...") 
+                urllib.request.urlretrieve(path, filename)
+        else:
+            print("已经存在"+filename)
+
+
+
+def getUserDecesion():
+    inputstr = input("请输入你要下载类型，预览图片回“1”，原图回“2”:")
+    while((inputstr != str(1)) and (inputstr != str(2))):
+        inputstr = input ("你输入的内容有误，请重新输入: ")
+    if(inputstr == str(1)):
+        print ("你选择的是: 预览图片")
+        return 1
+    elif (inputstr == str(2)):
+        print ("你选择的是: 原始图片")
+        return 2
+    else:
+        print("出现异常")
+        return 0
+
 #main method
+picVersion = getUserDecesion()
 totalPageNumber = getTotalPage(getHtml(urls))
+gotoDir(desPath)
 i = 0
 for i in range(totalPageNumber):
     url = urls+"page/"+str(i+1)
     i = i + 1
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     print(url)
-    getPic(getHtml(url))
+    if (picVersion == 1):
+        getPreviewPic(getHtml(url))
+    elif (picVersion == 2):
+        getDownloadPic(getHtml(url))
